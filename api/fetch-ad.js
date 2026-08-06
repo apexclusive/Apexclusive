69a565ae524e22509a1faea41906e1ece6e17394
apexclusive <putsmartijn@gmail.com>
2026-08-06 05:48:26 +0200

    Update fetch-ad.js

--- api/fetch-ad.js
+++ api/fetch-ad.js
@@ -73,9 +73,12 @@ export default async function handler(req, res) {
 
     const html = await response.text();
 
-    res.status(200).json({
-      html: html
-    });
+    const images = extractImages(html);
+
+res.status(200).json({
+  html: html,
+  images: images
+});
 
   } catch(e) {
 
